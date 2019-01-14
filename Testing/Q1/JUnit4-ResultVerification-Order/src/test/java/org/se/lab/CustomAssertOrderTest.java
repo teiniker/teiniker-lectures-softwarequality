344c34a75e637e9c1c48ae590e541e0b6c7b1acc
teiniker <egon.teiniker@gmail.com>
2019-01-14 20:49:14 +0100

    Refactoring.

--- Testing/Q1/JUnit4-ResultVerification-Order/src/test/java/org/se/lab/CustomAssertOrderTest.java
+++ Testing/Q1/JUnit4-ResultVerification-Order/src/test/java/org/se/lab/CustomAssertOrderTest.java
@@ -10,7 +10,7 @@ public class CustomAssertOrderTest
 	@Before
 	public void setup()
 	{
-	    // actual Order
+	    // actual Order (result of an exercise)
 		order = new Order(1, "Special order");
 		order.addOrderLine(new OrderLine(2, 1, new Product(3, "Pizza Frutti di Mare", 880)));
 		order.addOrderLine(new OrderLine(4, 2, new Product(5, "Pizza Quattro Formaggi", 820)));		
@@ -21,10 +21,16 @@ public class CustomAssertOrderTest
 	public void testOrder()
 	{
 		// verify
+	    Order expected = createExpectedOrder();
+	    CustomAssert.assertEquals(expected, order);
+	}
+		
+	
+	public Order createExpectedOrder()
+	{
 	    Order expected = new Order(1, "Special order");
 	    expected.addOrderLine(new OrderLine(2, 1, new Product(3, "Pizza Frutti di Mare", 880)));
 	    expected.addOrderLine(new OrderLine(4, 2, new Product(5, "Pizza Quattro Formaggi", 820)));
-
-	    CustomAssert.assertEquals(expected, order);
+	    return order;
 	}
 }
