819aaf7886ad7d111fd1d6022456a06f9c5840f9
teiniker <egon.teiniker@gmail.com>
2019-01-15 16:35:06 +0100

    Refactoring: added create() method.

--- Testing/Q1/JUnit4-Fixture-Transient/src/test/java/org/se/lab/ImplicitFixtureTest.java
+++ Testing/Q1/JUnit4-Fixture-Transient/src/test/java/org/se/lab/ImplicitFixtureTest.java
@@ -8,13 +8,22 @@ public class ImplicitFixtureTest
 {
 	private Order order;
 	
+	// Creation methods
+
+	private Order createNewOrder()
+	{
+		Order order = new Order(1, "Special order");
+		order.addOrderLine(new OrderLine(2, 1, new Product(3, "Pizza Frutti di Mare", 880)));
+		order.addOrderLine(new OrderLine(4, 2, new Product(5, "Pizza Quattro Formaggi", 820)));
+		return order;
+	}
+
+	
 	@Before
 	public void setup()
 	{
 	    // setup
-		order = new Order(1, "Special order");
-		order.addOrderLine(new OrderLine(2, 1, new Product(3, "Pizza Frutti di Mare", 880)));
-		order.addOrderLine(new OrderLine(4, 2, new Product(5, "Pizza Quattro Formaggi", 820)));
+		order = createNewOrder();
 	}
 	
 	
